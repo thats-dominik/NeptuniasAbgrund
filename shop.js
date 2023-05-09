fde7608650b3a2b11d719aff7c25dcb320a13636
dominik <dominik.haemmerle@lernende.bbw.ch>
2023-05-09 21:55:12 +0200

    tiere files have been created

---
+++ shop.js
@@ -0,0 +1,14 @@
+
+var imgs = document.querySelectorAll('.slider img');
+var dots = document.querySelectorAll('.dot');
+var currentImg = 0; // index of the first image
+const interval = 3000; // duration(speed) of the slide
+var timer = setInterval(changeSlide, interval);
+
+currentImg = (currentImg + 1) % imgs.length; // update the index number
+
+if (n != undefined) {
+    clearInterval(timer);
+    timer = setInterval(changeSlide, interval);
+    currentImg = n;
+}
